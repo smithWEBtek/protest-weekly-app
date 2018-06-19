12efc63b50477fc0524a802b8e5d6b97a0ce96d5
Susan Fowler <infopro94@gmail.com>
2018-06-19 11:36:04 -0500

    create welcome view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 	#validate cell phone
 	has_many :events
 
+#User model should have four attributes, all strings: name, email, image, and uid (the user's ID on Facebook)
+
 end
