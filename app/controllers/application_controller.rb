284f2bf068909b2be770b29a5395a1a93d8bbcbe
Susan Fowler <infopro94@gmail.com>
2018-06-19 11:57:26 -0500

    added current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+    before_action :current_user
+    before_action :require_logged_in, except: [:new, :create, :home]
+
+	private
+	def current_user
+    @current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+  	end
 end
