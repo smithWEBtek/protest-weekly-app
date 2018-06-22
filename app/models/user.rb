4630d4fb99fb089e6c9592732beaa0bcbafe86df
Susan Fowler <infopro94@gmail.com>
2018-06-22 16:20:06 -0500

    trying and failing at FB login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
-	# has_secure_password
+	
 	validates :name, presence: true
-	# validates :email, uniqueness: true, on: :create
 	has_many :events
 	has_many :organizations, through: :events
 
 
-#User model should have four attributes, all strings: name, email, image, and uid (the user's ID on Facebook)
-
-end
+	def self.from_omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  
+  end
