71b9523da87e8cf51a478e6be982724894edc22d
Susan Fowler <infopro94@gmail.com>
2018-06-23 11:48:48 -0500

    updated location of files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,5 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-# end
-
-OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['Google_client_id'], ENV['Google_client_secret'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :developer, unless Rails.env.production?
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
   root 'welcome#welcome'
   # get '/auth/facebook/callback' => 'sessions#create'
   
@@ -7,12 +9,12 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   post '/session', to: 'sessions#create'
   
-  get 'sessions/create'
+  # get 'sessions/create'
   get 'organizations/show'
   get 'organizations/new'
   post 'organizations/create'
   get 'venues/show'
-  delete '/signout', to: 'sessions#destroy'
+  # delete '/signout', to: 'sessions#destroy'
 
   resources :users
   resources :events
