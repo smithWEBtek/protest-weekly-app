f46d2dff2d95b658b10dd63f668b07d4a6e15757
Susan Fowler <infopro94@gmail.com>
2018-06-23 12:13:20 -0500

    added omniauth coding to facilitate FB and other provider logins

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,14 @@ module ProtestWeeklyApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options #source:https://github.com/omniauth/omniauth/blob/master/README.md
+  # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+  end 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  end
 end
-
