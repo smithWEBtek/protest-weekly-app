f25accd387741b6190259d1a970d752e2124c7ce
Susan Fowler <infopro94@gmail.com>
2018-06-24 15:18:48 -0500

    working on partial form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,11 +1,11 @@
 class OrganizationsController < ApplicationController
 
   def new
-	@organization = Organization.new
+	  @organization = Organization.new
   end
 
   def create
-  	@organization = Organization.find_or_create_by(id: params[:id])
+  	@organization = Organization.create(id: params[:id])
      render :show
   end
 
