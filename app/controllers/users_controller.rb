d0c7f03655052034bb5d02c07c85d5cac31d27a4
Susan Fowler <infopro94@gmail.com>
2018-06-30 14:37:10 -0500

    sorting out flow and views starting at the beginning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :password, :cell_phone, :email, event_users_attributes: [:datetime, :ride, :drive])
+		params.require(:user).permit(:name, :password, :cell_phone, :email, event_users_attributes: [:datetime, :need_ride, :can_drive])
 	end
 
 end
