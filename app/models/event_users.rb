c5b8a8127e3d28817fd4f80306a4c6d6e87312ee
Susan Fowler <infopro94@gmail.com>
2018-07-02 12:01:06 -0500

    three steps forwards, two steps back

--- app/models/event_users.rb
+++ app/models/event_users.rb
@@ -4,16 +4,16 @@ class EventUser < ApplicationRecord
 	belongs_to :user
 	#good candidate for scope to limit show returns of events for that week (sort by datetime)
 
-	def car_pool
-		@car_pool == @ride || @drive
+	def self.car_pool
+		self.car_pool == self.need_ride || self.can_drive
 	end
 
-	def need_ride
-		!can_drive
+	def self.need_ride
+		!self.can_drive
 	end
 
-	def can_drive
-		!need_ride
+	def self.can_drive
+		!self.need_ride
 	end
 
 end
