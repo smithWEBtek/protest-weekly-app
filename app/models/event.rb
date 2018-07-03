df52ca215feb05c12050cfb4ed9dccc7ea477726
Susan Fowler <infopro94@gmail.com>
2018-07-03 17:05:23 -0500

    more headway with organizations views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+	validates :name, presence: true
 	
 	has_many :event_users
 	has_many :users, through: :event_users
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
+	validates :name, presence: true
 	
 	has_many :events
 	
