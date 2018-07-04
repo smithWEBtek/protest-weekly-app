aedc4e2f7ca42153f89706f02d03441babf365b7
Susan Fowler <infopro94@gmail.com>
2018-07-04 11:19:49 -0500

    updated Gemfile and spec file

--- spec/features/creating_organizations_spec.rb
+++ spec/features/creating_organizations_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "Users can create new organizations" do 
 	scenario "with valid attributes" do 
@@ -7,7 +7,7 @@ RSpec.feature "Users can create new organizations" do
 		click_link "New Organization"
 
 		fill_in 'Name', with: 'Yada yada yada'
-		fill_in 'Contact_info', with: 'Whatever you want. Phone number, email, website - it's all good.
+		fill_in "Contact_info', with: 'Whatever you want."
 		click_button 'Create Organization'
 
 		expect(page).to have_content "Organization has been created."
