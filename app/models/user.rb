f780d6141747ab61236cb51e4ecf789e8e08d0fd
Susan Fowler <infopro94@gmail.com>
2018-07-04 15:23:36 -0500

    added attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ApplicationRecord
 	validates_uniqueness_of :email
   	validates :password, length: { in: 6..20 }
 
-  has_many :event_users
+  	has_many :event_users
 	has_many :events, through: :event_users
 	
 
+	def event_users_attributes=(event_users_attributes)
+		event_users_attributes.each do |event_user_attributes|
+			self.event_users.build(event_user_attributes)
+		end
+	end
 
+	def events_attributes=(events_attributes)
+		events_attributes.each do |events_attributes|
+			self.events.build(event_attributes)
+		end
+	end
+	
 	# def self.from_omniauth(auth)
  #      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
  #      user.provider = auth.provider
