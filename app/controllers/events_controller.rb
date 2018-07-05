6b55f936f71a6897c74cd24e1684ce2ef45c8311
Susan Fowler <infopro94@gmail.com>
2018-07-05 13:30:59 -0500

    successful event show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,7 +19,8 @@ class EventsController < ApplicationController
   end
 
   def index
-    @events = Event.all
+    # @events = Event.all
+    @events = Event.includes(:organization).all
   end
 
   def edit
