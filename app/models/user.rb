0da6474882d8be7dd644c1f874221b7bd623942b
Susan Fowler <infopro94@gmail.com>
2018-07-09 18:50:03 -0500

    began Omniauth signin with FB

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,18 +22,16 @@ class User < ApplicationRecord
 		end
 	end
 end 	
-	# def self.from_omniauth(auth)
- #      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
- #      user.provider = auth.provider
- #      user.uid = auth.uid
- #      user.name = auth.info.name
- #      user.oauth_token = auth.credentials.token
- #      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
- #      user.save!
- #    end
-  # example from has-many-through-forms-rails-labs-v-000
-  # has_many :comments
-  # has_many :posts, through: :comments
+	def self.from_omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 
 
 
