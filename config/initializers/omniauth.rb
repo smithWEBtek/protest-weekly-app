d992963c5d1093ced2120db84cdb3448b28e4241
Susan Fowler <infopro94@gmail.com>
2018-07-12 12:10:46 -0500

    resolved return hash by adding code to omniauth initializer file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer, unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] callback_url: "#{ENV["domain"]}/users/auth/facebook/callback", scope: 'email', token_params: { parse: :json } 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "#{ENV["domain"]}/users/auth/facebook/callback", scope: 'email', token_params: { parse: :json } 
   end
 # end
