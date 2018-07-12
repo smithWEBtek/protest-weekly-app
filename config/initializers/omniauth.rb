f53201957ae088d84f2dd1d8ac2501338ddd30c5
Susan Fowler <infopro94@gmail.com>
2018-07-12 13:18:37 -0500

    FB authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer, unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "https://localhost://3000", scope: 'email', token_params: { parse: :json } 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+   callback_url: "https://192.168.1.2/auth/facebook/callback", scope: 'email', 
+   token_params: { parse: :json } 
   end
 # end
