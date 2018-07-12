782343c182e37242848e8eb44c46c0b06aa71d3a
Susan Fowler <infopro94@gmail.com>
2018-07-12 17:15:49 -0500

    continued issues with eventusers

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -9,10 +9,11 @@ class EventUsersController < ApplicationController
 	end
 
 	def create
+		# @event_user = @user.event_users.build(event_user_params)
 		@event = Event.create(params[:event_id])
 		@user = User.create(params[:user_id])
 		@event_user = EventUser.create(event_user_params)
-		
+		byebug
 		if @event_user.attend || @event_user.car_pool
 			@event_user.save
 			redirect_to user_event_user_url(:id)
