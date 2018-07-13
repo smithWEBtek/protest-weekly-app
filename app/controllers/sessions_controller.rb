b3147d86cd24df8db75901fdb154df1bf77de08c
Susan Fowler <infopro94@gmail.com>
2018-07-13 17:45:09 -0500

    resolve errors caused by omniauth errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # @user = User.find_by(user_params)
+    # binding.pry
+    @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
-       session[:user_id] = @user.id
        redirect_to user_path(@user), notice: "Are you ready to make a difference?"
-       
+
+       session[:user_id] = @user.id
     else
         @user = User.from_omniauth(request.env["omniauth.auth"])
         session[:user_id] = @user.id
