b3e8ca66d6925f06450a8a8cf4a05949d5c09b53
Susan Fowler <infopro94@gmail.com>
2018-07-15 17:21:18 -0500

    FB login ends at Users show error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,11 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id   
        redirect_to user_path(@user)
-    elsif
-      @user = User.find_by(uid: auth['uid']) do |u|
-        u.name = auth['info']['name']
-        u.email = auth['info']['email']
-        u.image = auth['info']['image']
-        redirect_to signin_path
-       end
+    else
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        session[:user_id] = @user.id
+        redirect_to user_path(:user_id)
+       
      end
   end
     
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer, unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "https://localhost:3000/auth/facebook/callback", scope: 'email', token_params: { parse :json } 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "https://localhost:3000/", scope: 'email', token_params: { parse :json } 
   end
 end
