134106c3bb6440e05b90eacad1739b46895817eb
Susan Fowler <infopro94@gmail.com>
2018-07-17 17:43:43 -0500

    happenings, continued

--- app/models/happening.rb
+++ app/models/happening.rb
@@ -4,11 +4,11 @@ class Happening < ApplicationRecord
 	belongs_to :user
 	
 	def self.attend
-		!self.user && !self.event || self.car_pool		
+		!self.user && !self.event		
 	end
 
 	def self.car_pool
-		self.need_ride || self.can_drive
+		!self.need_ride || !self.can_drive
 	end
 
 	def self.need_ride
