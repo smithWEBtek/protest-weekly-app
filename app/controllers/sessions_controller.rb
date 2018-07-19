9871d827f4e2472654b0a02b2abe45aa2e99f954
Susan Fowler <infopro94@gmail.com>
2018-07-19 09:31:23 -0500

    changed sessions controller to allow local login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,27 @@ class SessionsController < ApplicationController
 	
   def new
   	@user = User.new
-    @user = current_user
+    # @user = current_user
   end
 
   def create
-      @user = User.find_or_create_by(name: params[:user][:name])
-    # if @user && @user.authenticate(params[:user][:password])
+       @user = User.find_or_create_by(name: params[:user][:name])
+        # @user && @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id   
-       # redirect_to user_path(@user)
+       redirect_to user_path(@user)
+
+      # if @user && @user.authenticate(params[:user][:password])
     # else
     #     @user = User.find_or_create_by(uid: auth['uid']) do |u|
     #       u.name = auth['info']['name']
     #       u.email = auth['info']['email']
     #       u.image = auth['info']['image']
-        # end
-        # session[:user_id] = @user.id
-        redirect_to user_path(:user_id)
-       
-     # end
+    #     end
+    # end
+
   end
-    
+     
+
   def destroy
     session.delete("user_id")
     redirect_to root_path
@@ -34,5 +35,5 @@ class SessionsController < ApplicationController
 	#   request.env['omniauth.auth']
 	# end
 
-  end
+ end 
 
