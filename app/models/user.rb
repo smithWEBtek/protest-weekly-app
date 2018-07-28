36dc630b671ce1381cf3166a54f9f1dbadd3b3ee
Susan Fowler <infopro94@gmail.com>
2018-07-28 13:50:31 -0500

    FB login working??

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_secure_password
 
 	validates :name, presence: true
-  	validates :password, presence: true, length: { in: 6..20 }
+  	validates :password, presence: true, length: { in: 6..64 }
 
   	has_many :happenings
 	has_many :events, through: :happenings
@@ -20,19 +20,19 @@ class User < ApplicationRecord
 		end
 	end
   
-	def generate_password
-    	self.password = loop do
-      	random_token = SecureRandom.urlsafe_base64
-        break random_token unless User.exists?(password: random_token)
-    end
-end
+# 	def generate_password
+#     	self.password = loop do
+#       	random_token = SecureRandom.urlsafe_base64
+#         break random_token unless User.exists?(password: random_token)
+#     end
+# end
 
 	# backup code
 	def self.from_omniauth(auth)
 	  email = auth[:info][:email] || "#{auth[:uid]}@facebook.com"
 	    @user = where(email: email).first_or_initialize
-	    @user.password = SecureRandom.hex
-	    @user.save && user
+	    @user.password = SecureRandom.urlsafe_base64
+	    @user.save && @user
 	  end
 	
  end
