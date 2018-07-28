ab8d65a090682e14bdd542e64aa5f7a4eeb2e3de
Susan Fowler <infopro94@gmail.com>
2018-07-28 12:52:41 -0500

    FB login

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,12 @@ class User < ApplicationRecord
 		end
 	end
   
-	# def self.from_omniauth(auth)
-	#   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	#     @user.email = auth.info.email
-	#     @user.password = Facebook.friendly_token[0,20]
-	#     @user.name = auth.info.name   # assuming the user model has a name
-	#     @user.image = auth.info.image # assuming the user model has an image
-	#   end
-	# end
+	def generate_password
+    	self.password = loop do
+      	random_token = SecureRandom.urlsafe_base64
+        break random_token unless User.exists?(password: random_token)
+    end
+end
 
 	# backup code
 	def self.from_omniauth(auth)
